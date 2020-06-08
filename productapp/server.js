d0538f21cfc66fa19e1376282c240eb947637911
Policharla Narasaiah <polnara1@in.ibm.com>
2020-06-08 19:38:47 +0530

    api implementation

--- productapp/server.js
+++ productapp/server.js
@@ -1,31 +1,71 @@
 const express = require('express');
-const http = require('http');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const path = require('path');
-
+const http = require('http');
 const app = express();
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,"dist")));
-// api to save user information
-// Post :  /user
-app.get("*",(req,res)=>{res.sendfile(path.join(__dirname,'dist/index.html'))})
-//app.get("/user",(req,res)=>{return {"username":"harish","firstName":"Harish","LastName":"mahesh","password":"Pass123"}})
+
+// API file for interacting with MongoDB
+//const api = require('./server/routes/api');
+const usersInfo = [
+    {"firstName":"Harish","lastName":"Mahesh","userName":"Harish","password":"Pass123","confirmpassword":"Pass123"}
+];
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 // get the user information
 //  Get : /user
 //  Get : /user/harish
+app.get("/user",(req, res) => { return res.json(usersInfo)})
+app.get("/user/:userName",(req,res)=>{ 
+    console.log(req.param("userName"));
+    for(var i = 0; i<usersInfo.length; i++){
+       if(req.param("userName") == usersInfo[i].userName){
+           console.log("It is Testing")
+           return res.json(usersInfo[i]);
+       }
+    }
+    return res.json({});
+ })
+
+app.post("/user",(req,res) => { 
+    usersInfo.push(req.body);
+    return res.json( 
+        req.body
+    );
+    // usersInfo.push(req) 
+})
 
 // update user information
 //  Update : /user
+app.put("/user",(req,res)=>{
+    for(var i = 0; i<usersInfo.length; i++){
+        if(req.body.userName == usersInfo[i].userName){
+            usersInfo.pop(usersInfo[i]);
+            break;
+        }
+    }
+    usersInfo.push(req.body);
+    return res.json(req.body);
+})
 
 // delete user information
 // Delete : /user
+//app.delete();
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 
-const port = process.env.port || '3000';
-app.set('port',port);
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port,()=>console.log("Running application at port "+port))
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
